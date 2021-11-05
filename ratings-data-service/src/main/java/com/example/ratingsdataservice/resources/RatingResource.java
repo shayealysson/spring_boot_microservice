f946624084503b9690c0635a1762c75381f1b32f
Shaye Baisac <shaye.baisac@rev-kitten.com>
2021-11-05 13:35:45 +0800

    Add UserRating model

--- ratings-data-service/src/main/java/com/example/ratingsdataservice/resources/RatingResource.java
+++ ratings-data-service/src/main/java/com/example/ratingsdataservice/resources/RatingResource.java
@@ -1,6 +1,10 @@
 package com.example.ratingsdataservice.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.example.ratingsdataservice.models.Rating;
+import com.example.ratingsdataservice.models.UserRating;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,5 +16,19 @@ public class RatingResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId){
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/users/{userId}")
+    public UserRating getRatingByUserId(@PathVariable("userId") String userId){
+        List<Rating> ratings = Arrays.asList(
+            new Rating("id1", 1),
+            new Rating("id2", 2),
+            new Rating("id3", 3),
+            new Rating("id4", 4),
+            new Rating("id5", 5)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
     }
 }
