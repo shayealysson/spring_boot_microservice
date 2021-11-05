0f4f6a26b27eb255804371a56bf84790972823d6
Shaye Baisac <shaye.baisac@rev-kitten.com>
2021-11-05 11:39:10 +0800

    Using Webclient for async http werb requst

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -12,25 +12,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 	
 	@Autowired
+	private WebClient.Builder wBuilder; 
+
+    @Autowired
 	private RestTemplate restTemplate; 
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        //RestTemplate restTemplate = new RestTemplate();
-        /*
-        Movie movie = restTemplate.getForObject(
-            //url, 
-            "http://localhost:8082/movies/shayed",
-            //responseType
-            Movie.class
-        );*/
-
 
         //get all rated movie ID's
         List<Rating> ratings = Arrays.asList(
@@ -43,12 +38,20 @@ public class MovieCatalogResource {
 
 
         return ratings.stream().map(rating -> {
+            Movie movie = wBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/"+rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+            
+                    /*
             Movie movie = restTemplate.getForObject(
                 //url, 
                 "http://localhost:8082/movies/"+rating.getMovieId(),
                 //responseType
                 Movie.class
-            );
+            );*/
             return new CatalogItem(movie.getName(), "test description", rating.getRating());
         })
             .collect(Collectors.toList());
