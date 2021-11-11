e88d67a9f6b8140ae488091ae43ddf949b604089
Shaye Baisac <shayebaisac@gmail.com>
2021-11-11 14:01:44 +0800

    Auto refresh config properties at runtime

--- spring-boot-config/src/main/java/com/example/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/com/example/springbootconfig/GreetingController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class GreetingController {
 
     @Value("${my.greeting1: Displaying defaul value, my.greeting1 does not exist in properties}")
